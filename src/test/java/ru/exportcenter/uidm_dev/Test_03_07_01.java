1a27f855f13c3b6a88455f776091b1026b7e4f52
maksimova.diana <maksimova.diana@otr.ru>
2022-03-16 10:20:35 +0300

    автотест Test_03_07_01 приведен в рабочее состояние

--- src/test/java/ru/exportcenter/uidm_dev/Test_03_07_01.java
+++ src/test/java/ru/exportcenter/uidm_dev/Test_03_07_01.java
@@ -3,18 +3,17 @@ package ru.exportcenter.uidm_dev;
 import com.codeborne.selenide.Condition;
 import framework.RunTestAgain;
 import functional.CommonFunctions;
+import functional.GUIFunctions;
 import io.qameta.allure.Description;
 import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class Test_03_07_01 extends Hooks_UIDM_DEV {
 
@@ -23,99 +22,110 @@ public class Test_03_07_01 extends Hooks_UIDM_DEV {
     @Link(name = "Test_03_07_01", url = "https://confluence.exportcenter.ru/pages/viewpage.action?pageId=117902466")
 
     @Test(retryAnalyzer = RunTestAgain.class)
-    public static void steps() {
+    public void steps() {
         step01();
         step02();
         step03();
         step04();
+        step05();
+        step06();
+        step07();
     }
 
     @Step("Перейти к оформлению сервиса «Логистика. Доставка продукции «Агроэкспрессом»»")
-    private static void step01() {
-        // Открыть браузер и перейти по ссылке http://uidm.uidm-dev.d.exportcenter.ru/ru/login
-        // Ввести логин и пароль demo_exporter/password
-        $(By.name("username")).sendKeys("demo_exporter");
-        $(By.name("password")).sendKeys("password");
-        $x("//button//*[text()='Войти']").click();
+    private void step01() {
+        //Ввести логин и пароль demo_exporter/password
+        new GUIFunctions().authorization("demo_exporter", "password");
 
-        webdriver().shouldHave(url("http://uidm.uidm-dev.d.exportcenter.ru/ru/main"), Duration.ofSeconds(30));
+        //Перейти во вкладку «Сервисы»
+        new GUIFunctions().selectTab("Сервисы")
+                .waitForURL("http://master-portal-dev.d.exportcenter.ru/services/business");
 
-        // Перейти во вкладку «Сервисы»
-        $x("//a[@class='header-menu-list__link '][text()='Сервисы']").click();
-        webdriver().shouldHave(url("http://master-portal-dev.d.exportcenter.ru/services/business"), Duration.ofSeconds(30));
+        //Выбрать сервис «Логистика. Доставка продукции «Агроэкспрессом»» и нажать кнопку «Оформить»
+        new GUIFunctions().waitForElementDisplayed("//*[contains(@class, 'preloader')]", 60)
+                .inputInSearchField("Поиск по разделу", "Логистика. Доставка продукции \"Агроэкспрессом\"")
+                .openSearchResult("Логистика. Доставка продукции \"Агроэкспрессом\"", "Оформить");
 
-        // Выбрать сервис «Логистика. Доставка продукции «Агроэкспрессом»» и нажать кнопку «Оформить»
-        $x("//input[@placeholder='Поиск по разделу']").sendKeys("Логистика\n");
-        $x("//div[text()='Логистика. Доставка продукции \"Агроэкспрессом\"']" +
-                "/ancestor::div[@class='support-card support-card--active p-3']//*[text()='Оформить']").click();
-
-        // Нажать на кнопку «Продолжить»
+        //Нажать на кнопку «Продолжить»
         switchTo().window(1);
-        $x("//*[text()='Логистика. Доставка продукции \"Агроэкспрессом\"']").shouldBe(Condition.visible, Duration.ofSeconds(60));
-        if (!$x("//button//*[text()='Продолжить']").exists()) {
+        refreshTab("//*[contains(text(), 'Продолжить')]", 15);
+        new GUIFunctions().clickWebElement("//*[contains(text(), 'Продолжить')]");
+    }
+
+    public void refreshTab(String expectedXpath, int times) {
+        for (int i = 0; i < times; i++) {
+            $x("//*[text()='Логистика. Доставка продукции \"Агроэкспрессом\"']").shouldBe(Condition.visible, Duration.ofSeconds(30));
+            if ($x(expectedXpath).isDisplayed()) {
+                break;
+            }
             refresh();
-            $x("//*[text()='Логистика. Доставка продукции \"Агроэкспрессом\"']").shouldBe(Condition.visible, Duration.ofSeconds(60));
+            CommonFunctions.wait(1);
         }
-        $x("//button//*[text()='Продолжить']").click();
     }
 
     @Step("Заполнить область «Информация о компании»")
-    private static void step02() {
-        // В поле «Почтовый адрес» вводим значение «Корнилаева 2»
-        input("Информация о компании", "Почтовый адрес", "Корнилаева 2");
-    }
-
-    private static void input(final String area, final String field, final String value) {
-        String inputAreaXPath = "//*[text()='" + area + "']/ancestor::div[not(@class)][1]//*[text()='" + field + "']/ancestor::div[not(@class)][1]";
-        String inputXPath = "";
-        int n = 1;
-
-        while (n < 10) {
-            inputXPath = inputAreaXPath + "//input[preceding::*[" + n + "][text()='" + field + "']]";
-            CommonFunctions.wait(1);
-            if ($x(inputXPath).exists())
-                break;
-            if (n == 0)
-                Assert.fail("Не найден элемент {By.xpath: " + inputXPath + "}");
-            n++;
-        }
-
-        $x(inputXPath).scrollIntoView(false);
-        $x(inputXPath).sendKeys(value);
-
-        // В поле корректно отображается введенное значение
-        Assert.assertEquals($x(inputXPath).getValue(), value);
-        // Нет сообщений об ошибке
-        Assert.assertFalse($x(inputAreaXPath + "//span[contains(@class, 'error')]").exists());
-    }
-
-    private static void setCheckboxOn(final String area, final String field) {
-        String checkboxAreaXPath = "//*[text()='" + area + "']/ancestor::div[not(@class)][1]//*[text()='" + field + "']/ancestor::div[not(@class)][1]";
-        String checkboxXPath = checkboxAreaXPath + "//div[contains(@class,'checkMark')]";
-
-        if ($x(checkboxAreaXPath + "//div[contains(@class,'checked')]/div").exists())
-            System.out.println("Параметр «" + field + "» уже был включен");
-        else
-            $x(checkboxXPath).scrollIntoView(false);
-            $x(checkboxXPath).click();
-
-        // В поле корректно отображается введенное значение
-        Assert.assertTrue($x(checkboxAreaXPath + "//div[contains(@class,'checked')]").exists());
-        // Нет сообщений об ошибке
-        Assert.assertFalse($x(checkboxAreaXPath + "//span[contains(@class, 'error')]").exists());
+    private void step02() {
+        new GUIFunctions().inputValueInArea("Информация о компании", "Почтовый адрес", "Корнилаева 2");
     }
 
     @Step("Заполнить область «Информация о заявителе»")
-    private static void step03() {
-        setCheckboxOn("Информация о заявителе", "Дополнительный контакт");
-        input("Информация о заявителе", "ФИО", "Иванов Иван Иванович");
-        input("Информация о заявителе", "Телефон", "+7(999)999-99-99");
-        input("Информация о заявителе", "Должность", "Менеджер");
-        input("Информация о заявителе", "Email", "word@mail.ru");
+    private void step03() {
+        new GUIFunctions().setCheckboxInArea("Информация о заявителе", "Дополнительный контакт", true)
+                .inputValueInArea("Информация о заявителе", "ФИО", "Иванов Иван Иванович")
+                .inputValueInArea("Информация о заявителе", "Телефон", "+7(999)999-99-99")
+                .inputValueInArea("Информация о заявителе", "Должность", "Менеджер")
+                .inputValueInArea("Информация о заявителе", "Email", "word@mail.ru");
     }
 
     @Step("Заполнить область «Информация для оказания услуги»")
-    private static void step04() {
+    private void step04() {
+        new GUIFunctions().selectValueFromDropdownInArea("Информация для оказания услуги", "Город отправления", "Ярославль")
+                .selectValueFromDropdownInArea("Информация для оказания услуги", "Город назначения", "Шанхай")
+                .setCheckboxInArea("Информация для оказания услуги", "Вывоз груза с адреса («Первая миля»)", true)
+                .inputValueInArea("Информация для оказания услуги", "Адрес", "Молодежная улица")
+                .inputValueInArea("Информация для оказания услуги", "Предполагаемая дата отправления груза", "22.10.2022");
+    }
+
+    @Step("Заполнить область «Информация о грузе»")
+    private void step05() {
+        new GUIFunctions().clickButtonInArea("Информация о грузе", "Добавить +")
+                .selectValueFromDropdownInArea("Сведения о продукции", "Наименование продукции", "кофе");
+        Assert.assertEquals($x("//*[@placeholder='Выберите код ТН ВЭД']").getValue(), "  Кофе");
+
+        new GUIFunctions().inputValueInArea("Сведения о продукции", "Вес продукции, кг", "532,000")
+                .selectValueFromDropdownInArea("Сведения о продукции", "Упаковка", "Фляги")
+                .inputValueInArea("Сведения о продукции", "Количество контейнеров", "156")
+                .selectValueFromDropdownInArea("Сведения о продукции", "Тип контейнера", "Универсальный")
+                .clickButtonInArea("Сведения о продукции", "Сохранить");
+    }
+
+    @Step("Заполнить область «Информация о грузополучателе»")
+    public void step06() {
+        CommonFunctions.printStep();
+        new GUIFunctions().inputValueInArea("Информация о грузополучателе", "Наименование грузополучателя", "Ss-password")
+                .inputValueInArea("Информация о грузополучателе", "Страна", "USA")
+                .inputValueInArea("Информация о грузополучателе", "Город", "Moscow")
+                .inputValueInArea("Информация о грузополучателе", "Дом", "Ff")
+                .inputValueInArea("Информация о грузополучателе", "Регион", "St-Peterburg")
+                .inputValueInArea("Информация о грузополучателе", "Район", "Raion")
+                .inputValueInArea("Информация о грузополучателе", "Улица", "Lenina street")
+                .inputValueInArea("Информация о грузополучателе", "Регистрационный номер грузополучателя", "223 22 44 2")
+                .inputValueInArea("Информация о грузополучателе", "Телефон", "+79999999999")
+                .inputValueInArea("Информация о грузополучателе", "Представитель грузополучателя", "Moscow disco rule")
+                .inputValueInArea("Информация о грузополучателе", "Email", "www@mail.ru");
+    }
+
+    @Step("Заполнить область «Дополнительные услуги»")
+    public void step07() {
+        CommonFunctions.printStep();
+        new GUIFunctions().setCheckboxInArea("Дополнительные услуги", "Вывоз груза с адреса («Первая миля»)", true)
+                .setCheckboxInArea("Дополнительные услуги", "Таможенное оформление", true)
+                .setRadiobuttonUnderСheckboxInArea("Дополнительные услуги", "Таможенное оформление", "РЭЦ")
+                .setCheckboxInArea("Дополнительные услуги", "Оформление ветеринарного сертификата", true)
+                .setRadiobuttonUnderСheckboxInArea("Дополнительные услуги", "Оформление ветеринарного сертификата", "РЭЦ")
+                .setCheckboxInArea("Дополнительные услуги", "Оформление фитосанитарного сертификата", true)
+                .setRadiobuttonUnderСheckboxInArea("Дополнительные услуги", "Оформление фитосанитарного сертификата", "РЭЦ")
+                .clickButton("Далее");
     }
 }
 
