81f89f20cde3b37e5231f0bf93fb15e1977e58cb
Илья <balashov.ilya@otr.ru>
2022-03-22 11:30:19 +0300

    фикс

--- src/test/java/ru/exportcenter/uidm_dev/agroexpress/Test_03_07_03.java
+++ src/test/java/ru/exportcenter/uidm_dev/agroexpress/Test_03_07_03.java
@@ -1,4 +1,4 @@
-package ru.exportcenter.uidm_dev;
+package ru.exportcenter.uidm_dev.agroexpress;
 
 import framework.RunTestAgain;
 import framework.Ways;
@@ -9,6 +9,7 @@ import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Step;
 import org.testng.annotations.Test;
+import ru.exportcenter.uidm_dev.Hooks_UIDM_DEV;
 
 import static com.codeborne.selenide.Selenide.*;
 
