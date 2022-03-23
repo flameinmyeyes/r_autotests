9b60e15598b572e4c8c27d89c566fc752f43de3c
Илья <balashov.ilya@otr.ru>
2022-03-23 17:26:39 +0300

    доработан FileFunctions.java

--- src/main/java/functional/FileFunctions.java
+++ src/main/java/functional/FileFunctions.java
@@ -1288,4 +1288,30 @@ public final class FileFunctions {
                 buttonName+"']//following-sibling::input[@type = 'file'] ").uploadFile(file);
     }
 
+    public static String readProperties(String filePath, String key) {
+
+        String value = "";
+
+        Properties properties = new Properties();
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "ANSI-1251");
+
+            if(filePath.endsWith(".xml")) {
+                properties.loadFromXML(fileInputStream);
+            } else {
+                properties.load(inputStreamReader);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Файл " + filePath + " не найден!");
+        }
+
+        value = properties.getProperty(key);
+
+        return value;
+    }
+
+
 }
