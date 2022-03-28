2e295dbd61cc5f1a17fb2f47847a6098bca75dd2
petrishchev.ruslan <ruslan2203>
2022-03-28 12:48:39 +0300

    Добавлен тест 02_08_06

--- src/test/java/ru/exportcenter/dev/patents/Test_02_08_06.java
+++ src/test/java/ru/exportcenter/dev/patents/Test_02_08_06.java
@@ -3,15 +3,15 @@ package ru.exportcenter.dev.patents;
 import framework.RunTestAgain;
 import functions.common.CommonFunctions;
 import functions.gui.GUIFunctions;
-import functions.gui.ext.Asserts;
 import io.qameta.allure.Description;
 import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Step;
-import org.openqa.selenium.Keys;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.exportcenter.dev.HooksDEV;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -22,9 +22,10 @@ public class Test_02_08_06 extends HooksDEV {
     @Link(name="Test_02_08_06", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=123868584")
 
     @Test(retryAnalyzer = RunTestAgain.class)
-    public void steps() {
+    public void steps() throws AWTException {
         step01();
         step02();
+        step03();
     }
 
     @Step("Авторизация")
@@ -35,41 +36,49 @@ public class Test_02_08_06 extends HooksDEV {
         new GUIFunctions().inContainer("Вход в личный кабинет")
                 .inField("Email").inputValue("demo_exporter")
                 .inField("Пароль").inputValue("password")
-                .clickButton("Войти");
+                .clickButton("Войти")
+                .waitForURL("http://uidm.uidm-dev.d.exportcenter.ru/ru/main");
+    }
 
-        new GUIFunctions().waitForURL("http://uidm.uidm-dev.d.exportcenter.ru/ru/main");
+    @Step("Навигация")
+    private void step02(){
+        CommonFunctions.printStep();
 
         //Перейти во вкладку «Сервисы»
         new GUIFunctions().selectTab("Сервисы")
                 .waitForURL("http://master-portal-dev.d.exportcenter.ru/services/business");
 
-        //Выбрать сервис «Компенсация части затрат на регистрацию ОИС за рубежом» и нажать кнопку «Подробнее»
+        //Выбрать сервис «Компенсация части затрат на регистрацию ОИС за рубежом»
         new GUIFunctions().waitForElementDisplayed("//input[@placeholder='Поиск по разделу']")
-                .inputInSearchField("Поиск по разделу", "Компенсация части затрат на регистрацию ОИС за рубежом").
-                clickByLocator("//div[text()='Государственные']")
-                .openSearchResult("Компенсация части затрат на регистрацию ОИС", "Подробнее");
+                .inputInSearchField("Поиск по разделу", "Компенсация части затрат на регистрацию ОИС за рубежом")
+                .clickByLocator("//div[text()='Государственные']");
     }
 
-    @Step("Получение информации о сервисе «Компенсация части затрат на регистрацию ОИС за рубежом»")
-    private void step02(){
+    @Step("Получение информации о сервисе")
+    private void step03() throws AWTException {
         CommonFunctions.printStep();
 
+        //Нажать кнопку «Подробнее»
+        new GUIFunctions().openSearchResult("Компенсация части затрат на регистрацию ОИС", "Подробнее");
+
         //Проверка корректного отображения заголовков «Компенсация части затрат на регистрацию ОИС за рубежом», разделы «Что получает экспортер?» и «Как получить».
-        Assert.assertEquals($x("//h1[contains(text(),'Компенсация части затрат на регистрацию ОИС за рубежом')]").getText(), "Компенсация части затрат на регистрацию ОИС за рубежом");
-        Assert.assertEquals($x("//*[text()='Что получает экспортер?']").getText(), "Что получает экспортер?");
-        Assert.assertEquals($x("//h2[text()='Как получить']").getText(), "Как получить");
+        new GUIFunctions().waitForElementDisplayed("//h1[contains(text(),'Компенсация части затрат на регистрацию ОИС за рубежом')]");
+        new GUIFunctions().waitForElementDisplayed("//*[text()='Что получает экспортер?']");
+        new GUIFunctions().waitForElementDisplayed("//h2[text()='Как получить']");
 
         //В боковом меню нажать  «Как получить»
-        $x("//a[text()='Как получить']").click();
+        new GUIFunctions().inContainer("Содержимое страницы")
+                .clickButton("Как получить");
 
         //Проверка отображения раздела «Как получить».
         new GUIFunctions().waitForElementDisplayed("//div[@class='timeline']");
 
         //В боковом меню нажать  «Описание»
-        new GUIFunctions().clickButton("Описание");
+        new GUIFunctions().inContainer("Содержимое страницы")
+                        .clickButton("Описание");
 
         //Проверка отображения заловока и раздела «Что получает экспортер?»
-        Assert.assertEquals($x("//*[text()='Что получает экспортер?']").getText(), "Что получает экспортер?");
+        new GUIFunctions().waitForElementDisplayed("//*[text()='Что получает экспортер?']");
     }
 }
 
