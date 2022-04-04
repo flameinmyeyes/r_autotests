cc888b9db2ffa402344f15e4f6efcf330ff2d162
vorozhko.aleksandr <vorozhko.aleksandr@otr.ru>
2022-04-04 16:34:23 +0300

    Реализован Test_03_07_02_1_10.java

---
+++ src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1_20.java
@@ -0,0 +1,100 @@
+package ru.exportcenter.test.agroexpress;
+
+import framework.RunTestAgain;
+import framework.Ways;
+import framework.integration.JupyterLabIntegration;
+import functions.api.RESTFunctions;
+import functions.common.CommonFunctions;
+import functions.common.DateFunctions;
+import functions.file.PropertiesHandler;
+import functions.gui.GUIFunctions;
+import io.qameta.allure.Description;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import net.sf.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import ru.exportcenter.test.HooksTEST;
+
+import java.util.Properties;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Test_03_07_02_1_20 extends HooksTEST {
+
+    public String WAY_TEST = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_20/";
+    public String WAY_TEST_PREVIOUS = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_10/";
+    private String processID;
+    private String docUUID;
+
+    @Owner(value="Ворожко Александр")
+    @Description("03 07 02.1.20 Получение результатов верификации от АО \"РЖД Логистика\"")
+    @Link(name="Test_03_07_02_1_10", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=123870742")
+
+    @Test(retryAnalyzer = RunTestAgain.class)
+    public void steps() {
+        WAY_TEST = setWay(WAY_TEST);
+        WAY_TEST_PREVIOUS = setWay(WAY_TEST_PREVIOUS);
+        precondition();
+        step01();
+        step02();
+    }
+
+    @AfterMethod
+    public void screenShot() {
+        CommonFunctions.screenShot(WAY_TEST);
+    }
+
+    @Step("Авторизация")
+    public void precondition() {
+        processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
+        RESTFunctions.getOrderStatus(processID);
+
+        if (!processID.equals("Черновик")) {
+            System.out.println("Перепрогон предыдущего теста");
+            Test_03_07_02_1_20 previous_test = new Test_03_07_02_1_20();
+            previous_test.steps();
+            processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
+        }
+    }
+
+    @Step("Авторизация")
+    public void step01() {
+        CommonFunctions.printStep();
+        docUUID = RESTFunctions.getOrderID(processID);
+
+        String token = RESTFunctions.getAccessToken();
+
+        JSONObject systemProp = new JSONObject();
+        systemProp.put("applicationId", docUUID);
+        systemProp.put("camundaId", "camunda-exp-search");
+        systemProp.put("processInstanceId", processID);
+        systemProp.put("status", "cost_calculation");
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("systemProp", systemProp);
+
+        RestAssured
+                .given()
+                        .baseUri("https://lk.t.exportcenter.ru")
+                        .basePath("/agroexpress-adapter/api/v1/response/order-status")
+                        .header("accept", "*/*")
+                        .header("Content-Type", "application/json")
+                        .header("Authorization", token)
+                        .body(requestBody)
+                .when()
+                        .post()
+                .then()
+                        .assertThat().statusCode(200);
+    }
+
+    @Step("Навигация")
+    public void step02() {
+        CommonFunctions.printStep();
+
+        Assert.assertEquals(RESTFunctions.getOrderStatus(processID), "Расчёт стоимости");
+    }
+}
