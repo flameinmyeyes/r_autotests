ad50a27edc584ded3ddb875f26e2509dbef9a256
Terebkov Andrei V <terebkov.andrei@otr.ru>
2022-04-07 18:16:17 +0300

    Изменение авто-теста.

--- src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1/Test_03_07_02_1_70.java
+++ src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1/Test_03_07_02_1_70.java
@@ -16,21 +16,21 @@ import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+import ru.exportcenter.test.HooksTEST;
 import ru.exportcenter.test.agroexpress.HooksTEST_agroexpress;
 
 import java.util.Properties;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class Test_03_07_02_1_70 extends HooksTEST_agroexpress {
+public class Test_03_07_02_1_70 extends HooksTEST {
+
 
     public String WAY_TEST = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1/Test_03_07_02_1_70/";
     public String WAY_TEST_PREVIOUS = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1/Test_03_07_02_1_60/";
     public String WAY_TO_PROPERTIES = WAY_TEST + "Test_03_07_02_1_70_properties.xml";
     public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
     private String processID;
-    private String token;
-    private String docUUID;
     private String docNum;
 
     @Owner(value="Теребкова Андрей")
@@ -39,10 +39,9 @@ public class Test_03_07_02_1_70 extends HooksTEST_agroexpress {
 
     @Test(retryAnalyzer = RunTestAgain.class)
     public void steps() {
-//      preconditions();
+      preconditions();
         step01();
         step02();
-
     }
 
     @AfterMethod
@@ -56,23 +55,30 @@ public class Test_03_07_02_1_70 extends HooksTEST_agroexpress {
         String status = RESTFunctions.getOrderStatus(processID);
         System.out.println(status);
 
-        if(!status.equals("Оплата счёта")) {
+        if (!status.equals("Оплата счёта")) {
             System.out.println("Перепрогон предыдущего теста");
 
-//            Test_03_07_02_1_60 test_03_07_02_1_60 = new Test_03_07_02_1_60();
-//            test_03_07_02_1_60.steps();
-//            CommonFunctions.wait(10);
-//            processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
+            Test_03_07_02_1_60 test_03_07_02_1_60 = new Test_03_07_02_1_60();
+            test_03_07_02_1_60.steps();
+            CommonFunctions.wait(20);
+            processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
         }
     }
 
     @Step("Авторизация")
     public void step01() {
         CommonFunctions.printStep();
-        token = RESTFunctions.getAccessToken("bpmn_admin");
+        new GUIFunctions()
+            .authorization(
+                PROPERTIES.getProperty("Логин")
+                ,PROPERTIES.getProperty("Пароль")
+                ,PROPERTIES.getProperty("Код")
+            )
+                .waitForLoading()
+                .closeAllPopupWindows();
     }
 
-    @Step("Навигация и отправка JSON-запроса в Swagger")
+    @Step("Навигация")
     public void step02() {
         CommonFunctions.printStep();
         docNum = JupyterLabIntegration.getFileContent("/files_for_tests/test/agroexpress/Test_03_07_02_1/Test_03_07_02_1_10/docNum.txt");
@@ -81,30 +87,19 @@ public class Test_03_07_02_1_70 extends HooksTEST_agroexpress {
 
         new GUIFunctions().clickByLocator("//*[@class='Button_text__3lYJC']");
 
-        new GUIFunctions().clickByLocator("//*[text()='№" + docNum + "']").waitForElementDisplayed("//div[text()='Статус']/following-sibling::div[text()='Подтверждение выбранных услуг']");
-
-        new GUIFunctions().closeAllPopupWindows().clickButton("Продолжить").waitForElementDisplayed("//*[text()='Расчет стоимости услуги']");
-    }
-
-    @Step("Авторизация")
-    public void step03() {
-        CommonFunctions.printStep();
-        open("https://lk.t.exportcenter.ru/ru/services/drafts/info/" + processID);
+//      Открыть заявку, проверить ставку
         new GUIFunctions()
-                .authorization(PROPERTIES.getProperty("Авторизация.Email"), PROPERTIES.getProperty("Авторизация.Пароль"), PROPERTIES.getProperty("Авторизация.Код"))
-                .waitForLoading()
-                .closeAllPopupWindows();
+                .clickByLocator("//*[text()='№" + docNum + "']")
+                .closeAllPopupWindows()
+                .waitForElementDisplayed("//div[text()='Статус']/following-sibling::div[text()='Оплата счёта']");
+
+//      Нажать кнопку «Продолжить»
+        new GUIFunctions()
+            .closeAllPopupWindows()
+            .clickButton("Продолжить")
+            .waitForElementDisplayed("//*[text()='Подтверждение оплаты счета']");
+
+//      Развернуть аккордеон "Подтверждение оплаты счета"
+        new GUIFunctions().clickButton("Подтверждение оплаты счета");
     }
-
-    @Step("Навигация")
-    public void step04() {
-        CommonFunctions.printStep();
-        new GUIFunctions().waitForURL("https://www.ya.ru");
-
-        new GUIFunctions().waitForElementDisplayed("//div[text()='Статус']/following-sibling::div[text()='Оплата счёта']");
-
-        JupyterLabIntegration.uploadTextContent(docUUID, WAY_TEST,"docUUID.txt");
-        JupyterLabIntegration.uploadTextContent(processID, WAY_TEST,"processID.txt");
-    }
-
 }
