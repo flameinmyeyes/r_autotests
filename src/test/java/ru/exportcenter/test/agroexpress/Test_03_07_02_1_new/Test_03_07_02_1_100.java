278bc81bb5d41f14d992c9c5c4e63357332316c6
Илья <balashov.ilya@otr.ru>
2022-04-07 18:35:56 +0300

    фикс сценария

--- src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_100.java
+++ src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_100.java
@@ -1,33 +1,26 @@
 package ru.exportcenter.test.agroexpress.Test_03_07_02_1_new;
 
-import com.google.gson.JsonObject;
 import framework.RunTestAgain;
 import framework.Ways;
 import framework.integration.JupyterLabIntegration;
 import functions.api.RESTFunctions;
 import functions.common.CommonFunctions;
-import functions.file.JSONHandler;
-import functions.file.PropertiesHandler;
 import io.qameta.allure.Description;
 import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Step;
-import io.restassured.RestAssured;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import ru.exportcenter.test.agroexpress.HooksTEST_agroexpress;
 
-import java.util.Properties;
-
 public class Test_03_07_02_1_100 extends HooksTEST_agroexpress {
 
     public String WAY_TEST = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_100/";
     public String WAY_TEST_PREVIOUS = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_90/";
-    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_03_07_02_1_100_properties.xml";
-    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
+//    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_03_07_02_1_100_properties.xml";
+//    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
     private String processID;
-
 
     @Owner(value="Ворожко Александр")
     @Description("03 07 02.1.100 Получение скорректированной заявки с расчетом (интеграция)")
--- src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_140.java
+++ src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_140.java
@@ -5,6 +5,7 @@ import framework.Ways;
 import framework.integration.JupyterLabIntegration;
 import functions.api.RESTFunctions;
 import functions.common.CommonFunctions;
+import functions.file.PropertiesHandler;
 import io.qameta.allure.Description;
 import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
@@ -12,19 +13,22 @@ import io.qameta.allure.Step;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import ru.exportcenter.test.agroexpress.HooksTEST_agroexpress;
+import ru.exportcenter.test.HooksTEST;
+import ru.exportcenter.test.agroexpress.Test_03_07_02_1.Test_03_07_02_1_130;
 
-public class Test_03_07_02_1_90 extends HooksTEST_agroexpress {
+import java.util.Properties;
 
-    public String WAY_TEST = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_90/";
-    public String WAY_TEST_PREVIOUS = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_80/";
-//    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_03_07_02_1_90_properties.xml";
+public class Test_03_07_02_1_140 extends HooksTEST {
+
+    private final String WAY_TEST = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_140/";
+    private final String WAY_TEST_PREVIOUS = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_130/";
+//    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_03_07_02_1_140_properties.xml";
 //    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
     private String processID;
 
-    @Owner(value="Ворожко Александр")
-    @Description("03 07 02.1.90 (A) Получение подтверждения от РЖДЛ о получении денежных средств")
-    @Link(name="Test_03_07_02_1_90", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=127895065")
+    @Owner(value="Балашов Илья")
+    @Description("03 07 02.1.140 (Р) Ответ от РЖДЛ с подтверждением о передаче закрывающих документов")
+    @Link(name="Test_03_07_02_1_140", url = "https://confluence.exportcenter.ru/pages/viewpage.action?pageId=127896179")
 
     @Test(retryAnalyzer = RunTestAgain.class)
     public void steps() {
@@ -37,19 +41,19 @@ public class Test_03_07_02_1_90 extends HooksTEST_agroexpress {
         CommonFunctions.screenShot(WAY_TEST);
     }
 
-    @Step("Предусловия")
+    @Step("Выполнение предусловий")
     public void precondition() {
         processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
         String status = RESTFunctions.getOrderStatus(processID);
         System.out.println(status);
 
-        if(!status.equals("Проверка оплаты")) {
+        if (!status.equals("")) {
             System.out.println("Перепрогон предыдущего теста");
 
-//            Test_03_07_02_1_80 test_03_07_02_1_80 = new Test_03_07_02_1_80();
-//            test_03_07_02_1_80.steps();
-//            CommonFunctions.wait(10);
-//            processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
+            ru.exportcenter.test.agroexpress.Test_03_07_02_1.Test_03_07_02_1_130 test_03_07_02_1_130 = new Test_03_07_02_1_130();
+            test_03_07_02_1_130.steps();
+            CommonFunctions.wait(20);
+            processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
         }
     }
 
@@ -59,15 +63,16 @@ public class Test_03_07_02_1_90 extends HooksTEST_agroexpress {
 
         String status = null;
         for (int i = 1; i < 20; i++) {
+            System.out.println("Идёт ожидание изменения статуса заявки...");
             status = RESTFunctions.getOrderStatus(processID);
-            if (status.equals("Передача груза")) {
+            if (status.equals("Услуга оказана")) {
                 break;
             } else {
                 CommonFunctions.wait(60);
             }
         }
 
-        Assert.assertEquals(status, "Передача груза");
+        Assert.assertEquals(status, "Услуга оказана");
 
         JupyterLabIntegration.uploadTextContent(processID, WAY_TEST,"processID.txt");
     }
