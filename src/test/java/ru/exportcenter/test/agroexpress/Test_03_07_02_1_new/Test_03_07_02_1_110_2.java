128b7d9116a2b4662a829094964c81ae1ff54437
Илья <balashov.ilya@otr.ru>
2022-04-07 23:47:27 +0300

    Доработано: Test_03_07_02_1_110_1.java Test_03_07_02_1_110_2.java Test_03_07_02_1_120.java

--- src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_110_2.java
+++ src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_110_2.java
@@ -20,26 +20,20 @@ import java.util.Properties;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class Test_03_07_02_1_110 extends HooksTEST_agroexpress {
+public class Test_03_07_02_1_110_2 extends HooksTEST_agroexpress {
 
-    public String WAY_TEST = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_110/";
+    public String WAY_TEST = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_110_2/";
     public String WAY_TEST_PREVIOUS = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1_new/Test_03_07_02_1_100/";
-    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_03_07_02_1_110_properties.xml";
-    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
     private String processID;
-    private String token;
-    private String docUUID;
 
     @Owner(value="Ворожко Александр")
-    @Description("03 07 02.1.110 Получение скорректированной заявки с расчетом (интеграция)")
-    @Link(name="Test_03_07_02_1_110", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=123882163")
+    @Description("03 07 02.1.110_2 Получение скорректированной заявки с расчетом (интеграция)")
+    @Link(name="Test_03_07_02_1_110_2", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=123882163")
 
     @Test(retryAnalyzer = RunTestAgain.class)
     public void steps() {
         precondition();
         step01();
-        step02();
-        step03();
     }
 
     @AfterMethod
@@ -50,7 +44,6 @@ public class Test_03_07_02_1_110 extends HooksTEST_agroexpress {
     @Step("Предусловия")
     public void precondition() {
         processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
-        docUUID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "docUUID.txt");
         String status = RESTFunctions.getOrderStatus(processID);
         System.out.println(status);
 
@@ -64,29 +57,8 @@ public class Test_03_07_02_1_110 extends HooksTEST_agroexpress {
         }
     }
 
-    @Step("Авторизация в ЕЛК")
-    public void step01() {
-        CommonFunctions.printStep();
-        open("https://lk.t.exportcenter.ru/ru/services/drafts/info/" + processID);
-        new GUIFunctions()
-                .authorization(PROPERTIES.getProperty("Авторизация.Email"), PROPERTIES.getProperty("Авторизация.Пароль"), PROPERTIES.getProperty("Авторизация.Код"))
-                .waitForLoading()
-                .closeAllPopupWindows();
-        refreshTab("//*[contains(text(), 'Продолжить')]", 60);
-    }
-
-    @Step("Навигация в ЕЛК")
-    public void step02() {
-        CommonFunctions.printStep();
-
-        new GUIFunctions()
-                .closeAllPopupWindows()
-                .clickButton("Продолжить")
-                .waitForElementDisplayed("//*[text() = 'Осуществляется перевозка']");
-    }
-
     @Step("Проверка изменения статуса заявки")
-    public void step03() {
+    public void step01() {
         CommonFunctions.printStep();
 
         String status = null;
@@ -102,17 +74,6 @@ public class Test_03_07_02_1_110 extends HooksTEST_agroexpress {
         Assert.assertEquals(status, "Формирование закрывающих документов");
 
         JupyterLabIntegration.uploadTextContent(processID, WAY_TEST,"processID.txt");
-    }
-
-    public void refreshTab(String expectedXpath, int times) {
-        for (int i = 0; i < times; i++) {
-            if($x(expectedXpath).isDisplayed()) {
-                break;
-            }
-            System.out.println("Refreshing");
-            refresh();
-            CommonFunctions.wait(1);
-        }
     }
 
 }
