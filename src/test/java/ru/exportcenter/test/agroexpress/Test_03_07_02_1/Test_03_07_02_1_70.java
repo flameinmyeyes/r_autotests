359dc30835a07fb65ccdd7a7e9b22cf0df682319
Terebkov Andrei V <terebkov.andrei@otr.ru>
2022-04-07 17:13:25 +0300

    Создание авто-теста.

---
+++ src/test/java/ru/exportcenter/test/agroexpress/Test_03_07_02_1/Test_03_07_02_1_70.java
@@ -0,0 +1,110 @@
+package ru.exportcenter.test.agroexpress.Test_03_07_02_1;
+
+import com.google.gson.JsonObject;
+import framework.RunTestAgain;
+import framework.Ways;
+import framework.integration.JupyterLabIntegration;
+import functions.api.RESTFunctions;
+import functions.common.CommonFunctions;
+import functions.file.JSONHandler;
+import functions.file.PropertiesHandler;
+import functions.gui.GUIFunctions;
+import io.qameta.allure.Description;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import ru.exportcenter.test.agroexpress.HooksTEST_agroexpress;
+
+import java.util.Properties;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Test_03_07_02_1_70 extends HooksTEST_agroexpress {
+
+    public String WAY_TEST = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1/Test_03_07_02_1_70/";
+    public String WAY_TEST_PREVIOUS = Ways.TEST.getWay() + "/agroexpress/Test_03_07_02_1/Test_03_07_02_1_60/";
+    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_03_07_02_1_70_properties.xml";
+    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
+    private String processID;
+    private String token;
+    private String docUUID;
+    private String docNum;
+
+    @Owner(value="Теребкова Андрей")
+    @Description("03 07 02.1.70 Получение скорректированной заявки с расчетом (интеграция)")
+    @Link(name="Test_03_07_02_1_70", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=123879143")
+
+    @Test(retryAnalyzer = RunTestAgain.class)
+    public void steps() {
+//      preconditions();
+        step01();
+        step02();
+
+    }
+
+    @AfterMethod
+    public void screenShot() {
+        CommonFunctions.screenShot(WAY_TEST);
+    }
+
+    @Step("Предусловия")
+    public void preconditions() {
+        processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
+        String status = RESTFunctions.getOrderStatus(processID);
+        System.out.println(status);
+
+        if(!status.equals("Оплата счёта")) {
+            System.out.println("Перепрогон предыдущего теста");
+
+//            Test_03_07_02_1_60 test_03_07_02_1_60 = new Test_03_07_02_1_60();
+//            test_03_07_02_1_60.steps();
+//            CommonFunctions.wait(10);
+//            processID = JupyterLabIntegration.getFileContent(WAY_TEST_PREVIOUS + "processID.txt");
+        }
+    }
+
+    @Step("Авторизация")
+    public void step01() {
+        CommonFunctions.printStep();
+        token = RESTFunctions.getAccessToken("bpmn_admin");
+    }
+
+    @Step("Навигация и отправка JSON-запроса в Swagger")
+    public void step02() {
+        CommonFunctions.printStep();
+        docNum = JupyterLabIntegration.getFileContent("/files_for_tests/test/agroexpress/Test_03_07_02_1/Test_03_07_02_1_10/docNum.txt");
+
+        System.out.println(docNum);
+
+        new GUIFunctions().clickByLocator("//*[@class='Button_text__3lYJC']");
+
+        new GUIFunctions().clickByLocator("//*[text()='№" + docNum + "']").waitForElementDisplayed("//div[text()='Статус']/following-sibling::div[text()='Подтверждение выбранных услуг']");
+
+        new GUIFunctions().closeAllPopupWindows().clickButton("Продолжить").waitForElementDisplayed("//*[text()='Расчет стоимости услуги']");
+    }
+
+    @Step("Авторизация")
+    public void step03() {
+        CommonFunctions.printStep();
+        open("https://lk.t.exportcenter.ru/ru/services/drafts/info/" + processID);
+        new GUIFunctions()
+                .authorization(PROPERTIES.getProperty("Авторизация.Email"), PROPERTIES.getProperty("Авторизация.Пароль"), PROPERTIES.getProperty("Авторизация.Код"))
+                .waitForLoading()
+                .closeAllPopupWindows();
+    }
+
+    @Step("Навигация")
+    public void step04() {
+        CommonFunctions.printStep();
+        new GUIFunctions().waitForURL("https://www.ya.ru");
+
+        new GUIFunctions().waitForElementDisplayed("//div[text()='Статус']/following-sibling::div[text()='Оплата счёта']");
+
+        JupyterLabIntegration.uploadTextContent(docUUID, WAY_TEST,"docUUID.txt");
+        JupyterLabIntegration.uploadTextContent(processID, WAY_TEST,"processID.txt");
+    }
+
+}
