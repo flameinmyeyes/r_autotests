83a0c91f2b17da1d1592c2d99a769f47f0df2a08
petrishchev.ruslan <ruslan2203>
2022-04-26 16:47:02 +0300

    Рыба автотеста финподдержки

--- src/test/java/ru/exportcenter/dev/finpodderzhka/Test_03_07_01.java
+++ src/test/java/ru/exportcenter/dev/finpodderzhka/Test_03_07_01.java
@@ -9,6 +9,8 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Step;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import static com.codeborne.selenide.Selenide.*;
 import ru.exportcenter.Hooks;
@@ -17,149 +19,89 @@ import java.util.Properties;
 
 public class Test_03_07_01  extends Hooks {
 
-    private String WAY_TEST = Ways.TEST.getWay() + "/patents/Test_02_08_02_3/";
-    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_02_08_02_3_properties.xml";
+    private String WAY_TEST = Ways.DEV.getWay() + "/finpodderzhka/Test_03_07_01/";
+    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_03_07_01_properties.xml";
     public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
 
     @Owner(value="Ворожко Александр")
     @Description("03 07 04 Сценарий 4")
     @Link(name="Test_03_07_04", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=117902512")
     @Test(retryAnalyzer = RunTestAgain.class)
-    public void steps() {
+    public void steps() throws InterruptedException {
         step01();
-//        step02();
-//        step03();
-//        step04();
-//        step05();
-//        step06();
-//        step07();
+        step02();
+        step03();
+        step04();
     }
 
-//    @AfterMethod
-//    public void screenShot() {
-//        CommonFunctions.screenShot(WAY_TEST + "screen.png");
-//    }
+    @AfterMethod
+    public void screenShot() {
+        CommonFunctions.screenShot(WAY_TEST + "screen.png");
+    }
 
     @Step("Авторизация")
-    public void step01() {
+    public void step01() throws InterruptedException {
         CommonFunctions.printStep();
 
 
-        open(PROPERTIES.getProperty("start_URL"));
+        open("http://arm-lkb.arm-services-dev.d.exportcenter.ru/");
 
         //Ввести логин и пароль demo_exporter/password
         new GUIFunctions()
-                .authorization(PROPERTIES.getProperty("Авторизация.Email"), PROPERTIES.getProperty("Авторизация.Пароль"))
-                .waitForURL("http://uidm.uidm-dev.d.exportcenter.ru/ru/main");
-
-//
-//        new GUIFunctions()
-//                .inContainer("Вход в личный кабинет")
-//                .inField("Email").inputValue("demo_exporter")
-//                .inField("Пароль").inputValue("password")
-//                .clickButton("Войти")
-//                .waitForURL("http://uidm.uidm-dev.d.exportcenter.ru/ru/main")
-//                .selectTab("Сервисы")
-//                .waitForURL("http://master-portal-dev.d.exportcenter.ru/services/business")
-//                .inputInSearchField("Поиск по разделу", "Логистика. Доставка продукции \"Агроэкспрессом\"")
-//                .openSearchResult("Логистика. Доставка продукции \"Агроэкспрессом\"", "Оформить")
-//                .switchPageTo(1)
-//                .waitForLoading();
-
-        refreshTab("//*[contains(text(), 'Продолжить')]", 20);
-        new GUIFunctions()
-                .clickButton("Продолжить");
+                .authorizationLib(PROPERTIES.getProperty("Авторизация.Email"), PROPERTIES.getProperty("Авторизация.Пароль"))
+                .waitForURL("http://arm-lkb.arm-services-dev.d.exportcenter.ru/");
     }
 
-    @Step("Заполнить область «Информация о компании»")
+    @Step("Сведения о продукте")
     public void step02() {
         CommonFunctions.printStep();
-        new GUIFunctions()
-                .inContainer("Информация о компании")
-                .inField("Почтовый адрес").inputValue("Корнилаева 2").assertValue().assertNoControl();
+
+        //Переход на вкладку продукты
+        $x("//a[@href='/products']").click();
+
+        //Создать новый продукт
+        $x("//span[text()='Создать новый продукт']").click();
     }
 
-    @Step("Заполнить область «Информация о заявителе»")
+    @Step("Сведения о продукте")
     public void step03() {
         CommonFunctions.printStep();
-        new GUIFunctions()
-                .inContainer("Информация о заявителе")
-                .inField("Дополнительный контакт").setCheckboxON()
-                .inField("ФИО").inputValue("Иванов Иван Иванович").assertValue().assertNoControl()
-                .inField("Телефон").inputValue("+7(999)999-99-99").assertValue().assertNoControl()
-                .inField("Должность").inputValue("Менеджер").assertValue().assertNoControl()
-                .inField("Email").inputValue("word@mail.ru").assertValue().assertNoControl();
+
+            //Тип продукта - "Финансирование"
+            //Категория продукта - "Инвестиционное финансирование"
+            $x("//span[text()='Тип продукта']/following::input").click();
+            CommonFunctions.wait(1);
+            $x("//span[text()='Тип продукта']/following::input").setValue("Финансирование");
+            CommonFunctions.wait(1);
+            $x("//span[text()='Тип продукта']/following::input").sendKeys(Keys.ENTER);
+            $x("//span[text()='Категория продукта']/following::input").click();
+            CommonFunctions.wait(1);
+            $x("//span[text()='Категория продукта']/following::input").setValue("Инвестиционное финансирование");
+            $x("//span[text()='Категория продукта']/following::input").sendKeys(Keys.ENTER);
+            new GUIFunctions().waitForElementDisplayed("//*[text()='Краткое описание продукта']");
+
+            $x("//span[text()='Целевое назначение']/following::textarea").setValue("Целевое назначение");
+            $x("//span[text()='Краткое описание продукта']/following::textarea").setValue("Краткое описание продукта");
+
+
+            new GUIFunctions().clickButton("Продолжить");
     }
 
-    @Step("Заполнить область «Информация для оказания услуги»")
+    @Step("Условия предоставления")
     public void step04() {
         CommonFunctions.printStep();
-        new GUIFunctions()
-                .inContainer("Информация для оказания услуги")
-                .inField("Город отправления").selectValue("Ульяновск").assertValue().assertNoControl()
-                .inField("Город назначения").selectValue("Харбин").assertValue().assertNoControl()
-                .inField("Вывоз груза с адреса («Первая миля»)").setCheckboxON()
-                .inField("Адрес").inputValue("Молодежная улица").assertValue().assertNoControl()
-                .inField("Предполагаемая дата отправления груза").inputValue("22.10.2022").assertValue().assertNoControl();
+
+//        new GUIFunctions().inField("Иностранный покупатель").setCheckboxON()
+//                .inField("Банк иностранного покупателя").setCheckboxON()
+//                .inField("Российский экспортёр").setCheckboxON()
+//                .inField("Российский банк").setCheckboxON();
+
     }
 
-    @Step("Заполнить область «Информация для оказания услуги»")
-    public void step05() {
-        CommonFunctions.printStep();
-        new GUIFunctions()
-                .inContainer("Информация о грузе")
-                .clickButton("Сборный груз")
-                .inField("Температурный режим на всю партию (от -30°C до 0°C или от 0°C до +30°C) ").setCheckboxON()
-                .inField("От").inputValue("-2").assertValue().assertNoControl()
-                .inField("До").inputValue("+30").assertValue().assertNoControl()
-                .clickButton("Добавить +")
 
-                //Модальный контейнер
-                .inContainer("Сведения о продукции")
-                .clickButton("Добавить новую")
-                .inField("Наименование продукции").inputValue("Продукция").assertValue().assertNoControl()
-                .inField("Код ТН ВЭД").selectValue("Сыры и творог").assertValue().assertNoControl()
-                .inField("Вес продукции, кг").inputValue("15,000").assertValue().assertNoControl()
-                .inField("Упаковка").selectValue("Поддоны").assertValue().assertNoControl()
-                .inField("Длина, см").inputValue("224").assertValue().assertNoControl()
-                .inField("Ширина, см").inputValue("45").assertValue().assertNoControl()
-                .inField("Высота, см").inputValue("122").assertValue().assertNoControl()
-                .inField("Количество грузовых мест, шт").inputValue("226").assertValue().assertNoControl()
-                .clickButton("Сохранить");
-    }
 
-    public void step06() {
-        CommonFunctions.printStep();
-        new GUIFunctions()
-                .inContainer("Информация о грузополучателе")
-                .inField("Наименование грузополучателя").inputValue("Ss-password").assertValue().assertNoControl()
-                .inField("Страна").inputValue("USA").assertValue().assertNoControl()
-                .inField("Город").inputValue("Moscow").assertValue().assertNoControl()
-                .inField("Дом").inputValue("Ff").assertValue().assertNoControl()
-                .inField("Регион").inputValue("St-Peterburg").assertValue().assertNoControl()
-                .inField("Район").inputValue("Raion").assertValue().assertNoControl()
-                .inField("Регистрационный номер грузополучателя").inputValue("223 22 44 2").assertValue().assertNoControl()
-                .inField("Телефон").inputValue("+79999999999").assertValue().assertNoControl()
-                .inField("Представитель грузополучателя").inputValue("Moscow disco rule").assertValue().assertNoControl()
-                .inField("Email").inputValue("www@mail.ru").assertValue().assertNoControl();
-    }
 
-    public void step07() {
-        CommonFunctions.printStep();
-        new GUIFunctions()
-                .inContainer("Дополнительные услуги")
-                .inField("Вывоз груза с адреса («Первая миля»)").setCheckboxON()
-                .inField("Адрес").assertValue("Молодежная улица")
-                .inField("Таможенное оформление").setCheckboxON().assertCheckboxON().assertNoControl()
-                .inField("РЖД Логистика").setRadiobuttonByDescription("Комплексная услуга таможенного оформления").assertRadiobuttonONByDescription()
-                .inField("Оформление ветеринарного сертификата").setCheckboxON().assertCheckboxON().assertNoControl()
-                .inField("РЖД Логистика").setRadiobuttonByDescription("Содействие в получении ветеринарных сертификатов").assertRadiobuttonONByDescription()
-                .inField("Оформление фитосанитарного сертификата").setCheckboxON().assertCheckboxON().assertNoControl()
-                .inField("РЖД Логистика").setRadiobuttonByDescription("Содействие в получении фитосанитарных сертификатов").assertRadiobuttonONByDescription();
 
-        new GUIFunctions()
-                .clickButton("Далее");
-    }
 
     public void refreshTab(String expectedXpath, int times) {
         for (int i = 0; i < times; i++) {
