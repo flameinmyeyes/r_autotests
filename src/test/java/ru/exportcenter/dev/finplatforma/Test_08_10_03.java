7733de01a69e5685f615ca561d31f9a9427b94c5
Terebkov Andrei V <terebkov.andrei@otr.ru>
2022-04-27 16:09:18 +0300

    1я цепочка. отладка

--- src/test/java/ru/exportcenter/dev/finplatforma/Test_08_10_03.java
+++ src/test/java/ru/exportcenter/dev/finplatforma/Test_08_10_03.java
@@ -2,6 +2,8 @@ package ru.exportcenter.dev.finplatforma;
 
 import framework.RunTestAgain;
 import framework.Ways;
+import framework.integration.JupyterLabIntegration;
+import functions.api.RESTFunctions;
 import functions.common.CommonFunctions;
 import functions.file.PropertiesHandler;
 import functions.gui.GUIFunctions;
@@ -13,34 +15,38 @@ import org.openqa.selenium.Keys;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import ru.exportcenter.Hooks;
+import ru.exportcenter.test.agroexpress.Test_03_07_03_1.Test_03_07_03_1_10;
+import ru.exportcenter.test.agroexpress.Test_03_07_03_1.Test_03_07_03_1_20;
 
 import java.awt.event.KeyEvent;
 import java.util.Properties;
-import java.util.Random;
 
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-public class Test_08_10_02 extends Hooks {
+public class Test_08_10_03 extends Hooks {
 
-    private String WAY_TEST = Ways.DEV.getWay() + "/finplatforma/Test_08_10_02/";
-    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_08_10_02_properties.xml";
-    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
-
+    private String WAY_TEST = Ways.DEV.getWay() + "/finplatforma/Test_08_10_03/";
+    public String WAY_TEST_FIRST = Ways.DEV.getWay() + "/finplatforma/Test_08_10_02/";
+    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_08_10_03_properties.xml";
     public String newProductName;
+
+    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
+    private String processID;
+    private String docUUID;
 
     @Owner(value="Петрищев Руслан, Теребков Андрей")
     @Description("08 10 02 Создание Черновика")
     @Link(name="Test_08_10_02", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=133412554")
     @Test(retryAnalyzer = RunTestAgain.class)
     public void steps() throws InterruptedException {
+        precondition();
         step01();
         step02();
-        step03();
-        step04();
-        step05();
-        step06();
-        step07();
+//        step03();
+//        step04();
+//        step05();
+//        step06();
+//        step07();
     }
 
     @AfterMethod
@@ -48,9 +54,22 @@ public class Test_08_10_02 extends Hooks {
         CommonFunctions.screenShot(WAY_TEST + "screen.png");
     }
 
+    @Step("Предусловия")
+    public void precondition() throws InterruptedException {
+        CommonFunctions.printStep();
+
+        Test_08_10_02 test_08_10_02 = new Test_08_10_02();
+        test_08_10_02.steps();
+        newProductName = test_08_10_02.newProductName;
+        CommonFunctions.wait(1);
+        closeWebDriver();
+    }
+
     @Step("Авторизация")
     public void step01() throws InterruptedException {
         CommonFunctions.printStep();
+
+        System.out.println(newProductName);
 
         open(PROPERTIES.getProperty("start_URL"));
 
@@ -70,58 +89,50 @@ public class Test_08_10_02 extends Hooks {
         CommonFunctions.wait(2);
         new GUIFunctions().clickButton("Создать новый продукт");
     }
-
-    @Step("Сведения о продукте")
-    public void step03() {
-        CommonFunctions.printStep();
-
-        //Тип продукта - "Финансирование"
-        newProductName = "Кредитование. Прямой кредит российскому банку " + getRandomName();
-        System.out.println(newProductName);
-
-        setValueInField(newProductName, "Наименование продукта");
-
-        System.out.println(newProductName);
-//        $x("//*[@id='fullNameOfProduct']").setValue(newProductName);
-
-//        new GUIFunctions().waitForURL("''''");
-        //Нажать на кнопку «Продолжить»
-        new GUIFunctions().clickButton("Продолжить");
-    }
-
-    @Step("Условия предоставления")
-    public void step04() {
-        CommonFunctions.printStep();
-
-        //Нажать на кнопку «Продолжить»
-        new GUIFunctions().clickButton("Продолжить");
-    }
-
-    @Step("Финансовые параметры")
-    public void step05() {
-        CommonFunctions.printStep();
-
-        //Нажать на кнопку «Продолжить»
-        new GUIFunctions().clickButton("Продолжить");
-    }
-
-    @Step("Особенности погашения")
-    public void step06() {
-        CommonFunctions.printStep();
-
-        //Нажать на кнопку «Сохранить как черновик»
-        new GUIFunctions().clickButton("Сохранить как черновик");
-        new GUIFunctions().waitForURL("''''");
-    }
-
-    @Step("Карточка продукта")
-    public void step07() {
-        CommonFunctions.printStep();
-
-        //Нажать на кнопку «Назад» (стрелкой)
-        $x("//img[@alt='Назад']").click();
-        new GUIFunctions().waitForElementDisplayed("//*[text()='Создать новый продукт']");
-    }
+//
+//    @Step("Сведения о продукте")
+//    public void step03() {
+//        CommonFunctions.printStep();
+//
+//        //Тип продукта - "Финансирование"
+//        setValueInField("Кредитование.\n" + "Прямой кредит российскому банку", "Наименование продукта");
+//
+//        //Нажать на кнопку «Продолжить»
+//        new GUIFunctions().clickButton("Продолжить");
+//    }
+//
+//    @Step("Условия предоставления")
+//    public void step04() {
+//        CommonFunctions.printStep();
+//
+//        //Нажать на кнопку «Продолжить»
+//        new GUIFunctions().clickButton("Продолжить");
+//    }
+//
+//    @Step("Финансовые параметры")
+//    public void step05() {
+//        CommonFunctions.printStep();
+//
+//        //Нажать на кнопку «Продолжить»
+//        new GUIFunctions().clickButton("Продолжить");
+//    }
+//
+//    @Step("Особенности погашения")
+//    public void step06() {
+//        CommonFunctions.printStep();
+//
+//        //Нажать на кнопку «Сохранить как черновик»
+//        new GUIFunctions().clickButton("Сохранить как черновик");
+//    }
+//
+//    @Step("Карточка продукта")
+//    public void step07() {
+//        CommonFunctions.printStep();
+//
+//        //Нажать на кнопку «Назад» (стрелкой)
+//        $x("//img[@alt='Назад']").click();
+//        new GUIFunctions().waitForElementDisplayed("//*[text()='Создать новый продукт']");
+//    }
 
     public void setValueInFieldFromSelect(String value, String field){
         $x("//span[text()='" + field + "']/following::input").click();
@@ -140,11 +151,5 @@ public class Test_08_10_02 extends Hooks {
         $x("//span[text()='" + field + "']/following::input").click();
         $x("//*[text()='" + field + "']//following::*[text()='" + value + "']//child::span[@class=\"ant-checkbox\"]").click();
         $x("//span[text()='" + field + "']/following::input").sendKeys(Keys.ESCAPE);
-    }
-    protected String getRandomName(){
-        String name = "";
-        for(int i=0; i<16; i++)
-            name = name + new Random().nextInt(9);
-        return name;
     }
 }
