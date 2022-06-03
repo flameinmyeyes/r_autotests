44534997c7c534dff5fc1d032bf8e3868850c9a3
petrishchev.ruslan <ruslan2203>
2022-06-03 10:52:25 +0300

    перенос на тест

---
+++ src/test/java/ru/exportcenter/test/finplatforma/Test_08_10_02.java
@@ -0,0 +1,117 @@
+package ru.exportcenter.test.finplatforma;
+
+import framework.RunTestAgain;
+import framework.Ways;
+import functions.common.CommonFunctions;
+import functions.file.PropertiesHandler;
+import functions.gui.GUIFunctions;
+import io.qameta.allure.Description;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Step;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import ru.exportcenter.Hooks;
+
+import java.util.Properties;
+import java.util.Random;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Test_08_10_02 extends Hooks {
+
+    private String WAY_TEST = Ways.DEV.getWay() + "/finplatforma/Test_08_10_02/";
+    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_08_10_02_properties.xml";
+    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
+
+    public String newProductName;
+
+    @Owner(value="Петрищев Руслан, Теребков Андрей")
+    @Description("08 10 02 Создание Черновика")
+    @Link(name="Test_08_10_02", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=133412554")
+    @Test(retryAnalyzer = RunTestAgain.class)
+    public void steps() throws InterruptedException {
+        step01();
+        step02();
+        step03();
+        step04();
+        step05();
+        step06();
+        step07();
+    }
+
+    @AfterMethod
+    public void screenShot() {
+        CommonFunctions.screenShot(WAY_TEST + "screen.png");
+    }
+
+    @Step("Авторизация")
+    public void step01() throws InterruptedException {
+        CommonFunctions.printStep();
+
+        open(PROPERTIES.getProperty("start_URL"));
+
+        //Ввести логин и пароль demo_exporter/password http://arm-lkb.arm-services-dev.d.exportcenter.ru/
+        new GUIFunctions()
+                .authorizationLib(PROPERTIES.getProperty("Авторизация.Email"), PROPERTIES.getProperty("Авторизация.Пароль"))
+                .waitForElementDisplayed("//a[@href='/products']");
+    }
+
+    @Step("Новигация")
+    public void step02() {
+        CommonFunctions.printStep();
+
+        //В области навигации нажать на раздел «Продукты»
+        //Нажать на кнопку «Создать новый продукт»
+        new GUIFunctions().clickButton("Продукты")
+                .waitForElementDisplayed("//*[text()='Создать новый продукт']");
+        new GUIFunctions().clickButton("Создать новый продукт");
+    }
+
+    @Step("Сведения о продукте")
+    public void step03(){
+        CommonFunctions.printStep();
+
+        //Тип продукта - "Финансирование"
+        CommonFunctions.wait(2);
+        newProductName = "Кредитование. Прямой кредит российскому банку " + new Random().nextInt(999999999);
+        new GUIFunctions().waitForLoading()
+                .setTextInField(newProductName, "Наименование продукта");
+
+        //Нажать на кнопку «Продолжить»
+        new GUIFunctions().clickButton("Продолжить");
+    }
+
+    @Step("Условия предоставления")
+    public void step04() {
+        CommonFunctions.printStep();
+
+        //Нажать на кнопку «Продолжить»
+        new GUIFunctions().clickButton("Продолжить");
+    }
+
+    @Step("Финансовые параметры")
+    public void step05() {
+        CommonFunctions.printStep();
+
+        //Нажать на кнопку «Продолжить»
+        new GUIFunctions().clickButton("Продолжить");
+    }
+
+    @Step("Особенности погашения")
+    public void step06() {
+        CommonFunctions.printStep();
+
+        //Нажать на кнопку «Сохранить как черновик»
+        new GUIFunctions().clickButton("Сохранить как черновик");
+    }
+
+    @Step("Карточка продукта")
+    public void step07() {
+        CommonFunctions.printStep();
+
+        //Нажать на кнопку «Назад» (стрелкой)
+        new GUIFunctions().clickByLocator("//img[@alt='Назад']")
+                .waitForElementDisplayed("//*[text()='Создать новый продукт']");
+    }
+}
---
+++ src/test/java/ru/exportcenter/test/finplatforma/Test_08_10_26.java
@@ -0,0 +1,83 @@
+package ru.exportcenter.test.finplatforma;
+
+import framework.RunTestAgain;
+import framework.Ways;
+import functions.common.CommonFunctions;
+import functions.file.PropertiesHandler;
+import functions.gui.fin.GUIFunctions;
+import io.qameta.allure.Description;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Step;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import ru.exportcenter.Hooks;
+
+import java.util.Properties;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Test_08_10_26 extends Hooks {
+
+    private String WAY_TEST = Ways.DEV.getWay() + "/finplatforma/Test_08_10_26/";
+    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_08_10_26_properties.xml";
+    public Properties PROPERTIES = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
+
+    @Owner(value="Петрищев Руслан")
+    @Description("08 10 26 Просмотр информации о Продукте")
+    @Link(name="Test_08_10_26", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=133422928")
+    @Test(retryAnalyzer = RunTestAgain.class)
+    public void steps() throws InterruptedException {
+        step01();
+        step02();
+        step03();
+    }
+
+    @AfterMethod
+    public void screenShot() {
+        CommonFunctions.screenShot(WAY_TEST + "screen.png");
+    }
+
+    @Step("Авторизация")
+    public void step01(){
+        CommonFunctions.printStep();
+
+        open(PROPERTIES.getProperty("start_URL"));
+
+        //Ввести логин и пароль
+        new GUIFunctions()
+                .authorization(PROPERTIES.getProperty("Авторизация.Email"), PROPERTIES.getProperty("Авторизация.Пароль"))
+                .waitForElementDisplayed("//*[text()='Наименование продукта']");
+    }
+
+    @Step("Навигация")
+    public void step02(){
+        CommonFunctions.printStep();
+
+        //В поле "Наименование продукта" ввести значение "Аккредитив. Резервный аккредитив"
+//        new GUIFunctions().inPlaceholder("Наименование продукта").inputValue(PROPERTIES.getProperty("Навигация.Наименование продукта"))
+//                .waitForLoading();
+
+    }
+
+    @Step("Просмотр информации о Продукте")
+    public void step03(){
+        CommonFunctions.printStep();
+
+        //Кликнуть по записи продукта с наименованием  "Аккредитив. Резервный аккредитив"
+        //Нажать на вкладку «Условия предоставления»
+        //Нажать на вкладку «Финансовые параметры»
+        //Нажать на вкладку «Особенности погашения»
+        new GUIFunctions().clickByLocator("(//tr[contains(@class,'ant-table-row')])[1]")
+                .waitForLoading();
+
+//        new GUIFunctions().clickButton("Аккредитив. Резервный аккредитив")
+//                .waitForLoading()
+        new GUIFunctions().clickButton("Условия предоставления")
+                .waitForElementDisplayed("//div[@class='ant-tabs-content ant-tabs-content-top']//following::*[contains(text(),'Условия предоставления')]")
+                .clickButton("Финансовые параметры")
+                .waitForElementDisplayed("//div[@class='ant-tabs-content ant-tabs-content-top']//following::*[contains(text(),'Финансовые параметры')]")
+                .clickButton("Особенности погашения")
+                .waitForElementDisplayed("//div[@class='ant-tabs-content ant-tabs-content-top']//following::*[contains(text(),'Особенности погашения')]");
+    }
+}
