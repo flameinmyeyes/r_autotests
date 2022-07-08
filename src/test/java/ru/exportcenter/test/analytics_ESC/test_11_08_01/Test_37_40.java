ce9c9e2930d86ea8fe0f0231cfcf48478e5e03f8
petrishchev.ruslan <ruslan2203>
2022-07-08 21:24:11 +0300

    Отладка скриптов генерации

--- src/test/java/ru/exportcenter/test/analytics_ESC/test_11_08_01/Test_37_40.java
+++ src/test/java/ru/exportcenter/test/analytics_ESC/test_11_08_01/Test_37_40.java
@@ -1,6 +1,7 @@
 package ru.exportcenter.test.analytics_ESC.test_11_08_01;
 
 import framework.RunTestAgain;
+import framework.Ways;
 import framework.integration.JupyterLabIntegration;
 import functions.common.CommonFunctions;
 import functions.gui.GUIFunctions;
@@ -11,28 +12,41 @@ import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 import ru.exportcenter.Hooks;
 
+import java.awt.*;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class Test_37_40 extends Hooks {
 
-    public String requestNumber = JupyterLabIntegration.getFileContent("/analytics_ESC/Test_03_07_01_1/requestNumber.txt");
+    public String WAY_TEST = Ways.TEST.getWay() + "/analytics_ESC/Test_03_07_01_1/";
+    public String requestNumber;
 
-    @Owner(value="***")
+    @Owner(value="Петрищев Руслан")
     @Description("***")
     @Link(name="Test_37_40", url="https://confluence.exportcenter.ru/pages/viewpage.action?pageId=138814859")
     @Test(retryAnalyzer = RunTestAgain.class)
-    public void steps() throws InterruptedException {
+    public void steps() throws InterruptedException, AWTException {
+        precondition();
         step01();
+    }
+
+    @Step("Предусловия")
+    public void precondition() throws InterruptedException, AWTException {
+        CommonFunctions.printStep();
+
+        Test_18_36 test_18_36 = new Test_18_36();
+        test_18_36.steps();
     }
 
     @Step("Авторизация")
     public void step01() {
 
+        requestNumber = JupyterLabIntegration.getFileContent(WAY_TEST + "requestNumber.txt");
+
         System.out.println("Шаг 37");
         open("https://lk.t.exportcenter.ru/");
         new GUIFunctions().authorization("demo_exporter", "password")
                 .waitForElementDisplayed("//*[contains(text(),'Показать все')]");
-
 
         System.out.println("Шаг 38");
         new GUIFunctions().clickByLocator("//*[contains(text(),'Показать все')]")
