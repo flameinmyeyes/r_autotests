9f1732c43189879cd10a519aa95ffdb7ff41c39a
Вадим Селедцов <seledtsov.vadim@otr.ru>
2022-09-28 17:04:21 +0300

    Test1000

---
+++ src/test/java/ru/exportcenter/dev/vet/Test_08_07_01.java
@@ -0,0 +1,87 @@
+package ru.exportcenter.dev.vet;
+
+import framework.RunTestAgain;
+import framework.Ways;
+import framework.integration.JupyterLabIntegration;
+import functions.common.CommonFunctions;
+import functions.common.DateFunctions;
+import functions.file.PropertiesHandler;
+import functions.gui.GUIFunctions;
+import io.qameta.allure.Description;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Step;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import ru.exportcenter.Hooks;
+
+import java.util.Properties;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Test_08_07_01 extends Hooks {
+
+    public String WAY_TEST = Ways.TEST.getWay() + "/vet/Test_08_07_01/";
+    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_08_07_01_properties.xml";
+    public Properties P = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
+    private String processID;
+
+    @Owner(value = "Селедцов Вадим")
+    @Description("08.07.01 Авторизация, для заявки Запрос разрешения на вывоз подконтрольной продукции")
+    @Link(name = "Test_08_07_01", url = "https://confluence.exportcenter.ru/pages/viewpage.action?pageId=175264999")
+    @Test(retryAnalyzer = RunTestAgain.class)
+    public void steps() {
+        step01();
+        step02();
+        step03();
+
+    }
+
+    @AfterMethod
+    public void screenShot() {
+        CommonFunctions.screenShot(WAY_TEST);
+    }
+
+    @Step("Авторизация")
+    public void step01() {
+        CommonFunctions.printStep();
+        open(P.getProperty("start_URL"));
+
+        //Ввести логин и пароль demo_exporter/password
+        new GUIFunctions()
+                .authorization(P.getProperty("Авторизация.Email"), P.getProperty("Авторизация.Пароль"))
+                //.waitForURL("http://uidm.uidm-dev.d.exportcenter.ru/ru/main");
+                .waitForElementDisplayed("//span[text()='Запрос разрешения на вывоз подконтрольной продукции']")
+                .refreshTab("Продолжить", 20)  // это бага , кнопка продолжить не отображается с первого раза, перезагрузка страницы 10 раз
+                .clickButton("Продолжить")
+                .waitForElementDisplayed("//span[text()='Запрос разрешения на вывоз подконтрольной продукции']");
+    }
+
+    @Step("Выбрать Запрос разрешения на вывоз подконтрольной продукции")
+    public void step02() {
+        CommonFunctions.printStep();
+
+        //Выбрать Запрос разрешения на вывоз подконтрольной продукции
+        new GUIFunctions()
+                .clickByLocator("//span[text()='Запрос разрешения на вывоз подконтрольной продукции']")
+                .clickButton("Продолжить")
+                .waitForElementDisplayed("//div[text()='Информация о заявителе']");
+    }
+
+    @Step("Проверить корректность предзаполненных данных")
+    public void step03() {
+        CommonFunctions.printStep();
+
+        //Проверить корректность предзаполненных данных
+
+        $x("//*[text() = 'Информация о заявителе']/ancestor::div[contains(@class, 'container')][1]//span[text()='ИНН']").shouldHave(text("7743300600"));
+        $x("//*[text() = 'Информация о заявителе']/ancestor::div[contains(@class, 'container')][1]//span[text()='КПП']").shouldHave(text("287545403"));
+        $x("//*[text() = 'Информация о заявителе']/ancestor::div[contains(@class, 'container')][1]//span[text()='ОГРН']").shouldHave(text("1157746363994"));
+        $x("//*[text() = 'Информация о заявителе']/ancestor::div[contains(@class, 'container')][1]//span[text()='Контактный телефон']/following-sibling::span[1]").shouldHave(text("79787677078"));
+        $x("//*[text() = 'Информация о заявителе']/ancestor::div[contains(@class, 'container')][1]//span[text()='Email']/following-sibling::span[1]").shouldHave(text("primakov.aleksandr@otr.ru"));
+        $x("//*[text() = 'Информация о заявителе']/ancestor::div[contains(@class, 'container')][1]//span[text()='Наименование организации']").shouldHave(text(" ООО \"АЛСЕЗА\""));
+
+    }
+
+}
