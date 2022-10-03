666b857f861eed247951591133b3e227b39d2f7d
petrishchev.ruslan <ruslan2203>
2022-10-03 12:46:16 +0300

    Актуализация, добавлен автотест

--- src/test/java/ru/exportcenter/test/apk/Test_02_07_00/Test_02_07_00_2.java
+++ src/test/java/ru/exportcenter/test/apk/Test_02_07_00/Test_02_07_00_2.java
@@ -3,6 +3,7 @@ package ru.exportcenter.test.apk.Test_02_07_00;
 import framework.RunTestAgain;
 import framework.Ways;
 import functions.common.CommonFunctions;
+import functions.gui.GUIFunctions;
 import functions.gui.lkb.GUIFunctionsLKB;
 import io.qameta.allure.Description;
 import io.qameta.allure.Link;
@@ -11,18 +12,20 @@ import io.qameta.allure.Step;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import ru.exportcenter.Hooks;
+
 import java.awt.*;
-import static com.codeborne.selenide.Selenide.open;
 
-public class Test_02_07_00_1 extends Hooks {
+import static com.codeborne.selenide.Selenide.*;
 
-    private String WAY_TEST = Ways.TEST.getWay() + "/apk/Test_02_07_00/Test_02_07_00_1/";
+public class Test_02_07_00_2 extends Hooks {
+
+    private String WAY_TEST = Ways.TEST.getWay() + "/apk/Test_02_07_00/Test_02_07_00_2/";
 //    public String WAY_TO_PROPERTIES = WAY_TEST + "Test_01_07_01_1_properties.xml";
 //    public Properties P = PropertiesHandler.parseProperties(WAY_TO_PROPERTIES);
 
     @Owner(value = "Петрищев Руслан")
-    @Description("02 07 00.1 Авторизация в МДМ")
-    @Link(name = "Test_02_07_00_1", url = "https://confluence.exportcenter.ru/pages/viewpage.action?pageId=175248692")
+    @Description("02 07 00.2 Авторизация в bpms")
+    @Link(name = "Test_02_07_00_2", url = "https://confluence.exportcenter.ru/pages/viewpage.action?pageId=183188555")
     @Test(retryAnalyzer = RunTestAgain.class)
     public void steps() throws AWTException {
         step01();
@@ -33,14 +36,12 @@ public class Test_02_07_00_1 extends Hooks {
         CommonFunctions.screenShot(WAY_TEST + "screen.png");
     }
 
-    @Step("Авторизация")
+    @Step("Авторизация в МДМ")
     public void step01() {
         CommonFunctions.printStep();
 
-        //Ввести логин и пароль, нажать «Войти»
-        open("https://mdm.t.exportcenter.ru/");
-        new GUIFunctionsLKB()
-                .authorization("mdm_admin", "password")
-                .waitForURL("https://mdm.t.exportcenter.ru/catalog");
+        open("https://bpms.t.exportcenter.ru/main");
+        new GUIFunctionsLKB().authorization("bpmn_admin", "password")
+                .waitForURL("https://bpms.t.exportcenter.ru/main");
     }
 }
